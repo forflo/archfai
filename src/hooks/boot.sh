1a128170e209e60822bae35a0d0ca0d9506cf039
forflo <forflo@users.noreply.github.com>
2014-10-09 01:03:44 +0200

    Update boot.sh

--- src/hooks/boot.sh
+++ src/hooks/boot.sh
@@ -4,7 +4,7 @@
 ##
 boot_hook(){
 	local partnum="1"
-	local dev_uuid=$(lsblk --output "NAME,UUID" | grep ${BS_DISK}${partnum} | awk '{print $2}')
+	local dev_uuid=$(lsblk --output "NAME,UUID" | grep ${BS_DISK:5:3}${partnum} | awk '{print $2}')
 	
 	[ "${dev_uuid}" = "" ] && {
 		clog 1 "[boot_hook]" Could not query the UUID for ${BS_SP:5:4}
