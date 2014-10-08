b4f25c693ce97eb05cc2ca9e30f9404a4f9b2823
forflo <forflo@users.noreply.github.com>
2014-10-08 14:38:01 +0200

    Update crypt.sh

--- src/hooks/crypt.sh
+++ src/hooks/crypt.sh
@@ -5,5 +5,26 @@
 # in this function.
 ##
 crypt_hook(){
-	:
+	local crypt_device="cryptroot"
+	
+	clog 2 "crypt_hook()" Doing cryptsetup with LUKS.
+	# requires the user to break the unattendedness of this script package
+	cryptsetup --verbose --key-size=512\
+		--hash=sha512 --cipher=serpent-xts-plain64 \
+		--verify-passphrase --use-urandom luksFormat ${BS_SP} || {
+			
+		clog 1 "crypt_hook()" Cryptsetup failed!
+		return 1		
+	}
+	
+	cryptsetup open ${BS_SP} ${crypt_device} || {
+		clog 1 "crypt_hook()" Opening and creation of mapping device failed!
+		return 1
+	}
+	
+	# Sets the system partition to the new layer that
+	# sits on top of the old BS_SP
+	BS_SP="/dev/mapper/${crypt_device}"
+	
+	return 0
 }
