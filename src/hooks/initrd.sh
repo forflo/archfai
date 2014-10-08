ec846d3de159f0c770086ad1d59eedf4747d1b33
florian <mayer.florian@web.de>
2014-10-09 00:23:12 +0200

    Added important comment

--- src/hooks/initrd.sh
+++ src/hooks/initrd.sh
@@ -3,6 +3,10 @@
 # init rd configuration hook which can
 # be used to manipulate the input of
 # mkinitcpio.
+# Please note that you have to use either
+# env_execChroot or env_execChrootH (both
+# defined in env.conf) to modify the configuration
+# in the already bootstrapped environment!
 ##
 initrd_hook(){
 :
--- src/hooks/net.sh
+++ src/hooks/net.sh
@@ -1,6 +1,10 @@
 
 ##
-# Network configuration hook
+# Network configuration hook.
+# Please note that you have to use either
+# env_execChroot or env_execChrootH (both
+# defined in env.conf) to modify the configuration
+# in the already bootstrapped environment!
 ##
 net_hook(){
   :
