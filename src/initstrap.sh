20c4298e4d2a21bed4624b04f810a03398628ff1
Florian Mayer <mayerflorian@me.com>
2014-10-24 17:32:02 +0200

    Refactoring progress

--- src/initstrap.sh
+++ src/initstrap.sh
@@ -41,6 +41,31 @@ AF_FILES=(
 	"hooks/boot_hook.sh"
 )
 
+archfai_help(){
+	cat << EOF
+Archfai help
+============
+usage: $0 	[ -h | -l | -v ]
+	v := Puts the current version on the console
+	h := Displays help
+	l := Toggles the local mode 
+		 (see https://github.com/forflo/archfai)
+EOF
+	return 0
+}
+
+archfai_version(){
+	cat << EOF
+Version: ${VERSION}
+===================
+Archfai aims to provide a shell script package
+specific for archlinux which makes it possible
+to automate the  complete installation process
+with just one command.
+EOF
+	return 0
+}
+
 ##
 # Downloads and sources the needed scripts
 ##
@@ -64,7 +89,18 @@ archfai_init(){
 			return 1
 		}
 	done
-	clog 2 "[is_download()]" Finished successfully.
+
+	##
+	# Sourcing optional File 
+	[ -f "${OPT_SETTINGS}" ] && {
+		clog 3 "[archfai_init()]" Found local settings File! Will now load it.
+		. ${OPT_SETTINGS} || {
+			clog 1 "[archfai_init()]" "Something failed while sourcing ${OPT_SETTINGS}"
+			exit 1
+		}
+		clog 3 "[archfai_init()]" Loading finished successfully!
+	}
+	clog 2 "[archfai_init()]" Finished successfully.
 
 	return 0
 }
@@ -76,23 +112,12 @@ archfai_init(){
 # Return:
 #   <void>
 ##
-is_startStrapping(){
+archfai_startStrapping(){
 	[ "$1" = "local" ] && {
 		env_loadHooksLocal
 	} || {
 		env_loadHooks
 	}
-	
-#	clog 2 "[is_startStrappig()]" Loading ${IS_NAMES[0]}
-#	. ${IS_NAMES[0]} || {
-#		clog 1 "[is_startStrappig()]" Loading ${IS_NAMES[0]} failed!
-#		return 1
-#	}
-	
-#	clog 2 "[is_startStrapping()]" Loading ${IS_NAMES[1]}
-#	. ${IS_NAMES[1]} || {
-#		clog 1 "[is_startStrappig()]" Loading ${IS_NAMES[1]} failed!
-#	}
 	
 	# bs_install is the function that's been sourced by IS_NAMES[0]
 	bs_install || {
@@ -105,151 +130,70 @@ is_startStrapping(){
 		return 1
 	}
 	
-	flog 2 bold blink "[is_startStrapping()]" Finished bootstrapping! "You're good to go!"
+	flog 2 bold under "[archfai_startStrapping()]" Finished bootstrapping! "You're good to go!"
 
 	return 0
 }
-
-# currently not used
-###
-## Deletes each downloaded file
-#archfai_clean(){
-#	for ((i=0; i<${#IS_NAMES[*]}; i++)); do
-#		rm ${IS_NAMES[i]} > /dev/null 2>&1 || {
-#			clog 1 "[is_clean()]" Deletion of ${IS_NAMES[i]} failed!
-#			return 1 
-#		}
-#	done
-#	
-#	rm env.conf > /dev/null 2>&1 || {
-#		clog 1 "[is_clean()]" Deletion of env.conf failed!
-#		return 1
-#	}
-#	return 0
-#}
 
 ##
 # Starting point
 ##
-is_startOnline(){
-	is_loadEnv || {
-		clog 1 "[is_startOnline()]" Could not download environment file.
-		exit 1
-	}
-	
-	# look for additional local settings
-	[ -f "${OPT_SETTINGS}" ] && {
-		clog 3 "[is_startOnline()]" Found local settings File! Will now load it.
-		. ${OPT_SETTINGS} || {
-			clog 1 "[is_startOnline()]" "Something failed while sourcing ${OPT_SETTINGS}"
-			exit 1
-		}
-		clog 3 "[is_startOnline()]" Loading finished successfully!
+archfai_startOnline(){
+	archfai_startStrapping || {
+		clog 1  "[is_startOnline()]" Could not start bootstrapping.
+		return 1
 	}
 
-	is_download || {
-		clog 1 "[is_startOnline()]" Could not download bootstrapping files.
-		exit 1
-	}
-	
-	is_startStrapping || {
-		clog 1  "[is_startOnline()]" Could not start bootstrapping.
-		is_clean || {
-			clog 1 "[is_startOnline()]" Could not clean environment.
-			exit 1
-		}
-		exit 1
-	}
-	
-	is_clean || {
-		clog 1 "[is_startOnline()]" Could not clean environment.
-		exit 1
-	}
-	
-	exit 0
+	return 0
 }
 
 archfai_startLocal(){
 	# 1) Checking for primary files
-	for i in ${LOCAL_NAMES[*]}; do
-		echo "[is_startLocal()]" Checking for file $i.
+	for i in ${AF_FILES[*]}; do
+		echo "[archfai_startLocal()]" Checking for file $i.
 		[ -f $i ] && {
-			echo "[is_startLocal()]" File $i OK.
+			echo "[archfai_startLocal()]" File $i OK.
 		} || {			
 			[ "$i" = "bootstrap" -o "$i" = "chrootstrap" ] && {
-				echo "[is_startLocal()]" Mandatory file not found: $i.
+				echo "[archfai_startLocal()]" Mandatory file not found: $i.
 				exit 0
 			}
 		}
 	done
-	
+
 	# 2) Loading env.conf
 	[ -f env.conf ] && {
-		echo "[is_startLocal()]" Loading env.conf.
+		echo "[archfai_startLocal()]" Loading env.conf.
 		chmod 750 env.conf || {
-			echo "[is_startLocal()]" Chmod failed!
-			exit 1
+			echo "[archfai_startLocal()]" Chmod failed!
+			return 1
 		}
 		
 		. env.conf || {
-			echo "[is_startLocal()]" Something failed while sourcing env.conf!
-			exit 1
+			echo "[archfai_startLocal()]" Something failed while sourcing env.conf!
+			return 1
 		}
 	} || {
-		echo "[is_startLocal()]" env.conf is missing!
-		exit 1
+		echo "[archfai_startLocal()]" env.conf is missing!
+		return 1
 	}
-	
+
 	# 3) Loading additional settings
 	[ -f "${OPT_SETTINGS}" ] && {
-		clog 3 "[is_startOnline()]" Found local settings File! Will now load it.
+		clog 3 "[archfai_startLocal()]" Found local settings File! Will now load it.
 		. ${OPT_SETTINGS} || {
-			clog 1 "[is_startOnline()]" "Something failed while sourcing ${OPT_SETTINGS}!"
-			exit 1
+			clog 1 "[archfai_startLocal()]" "Something failed while sourcing ${OPT_SETTINGS}!"
+			return 1
 		}
-		clog 3 "[is_startOnline()]" Loading finished successfully!
+		clog 3 "[archfai_startLocal()]" Loading finished successfully!
 	}
-	
+
 	# 4) Start bootstrap.sh
-	is_startStrapping "local" || {
-		clog 1  "[is_startOnline()]" Could not start bootstrapping.
-		is_clean || {
-			clog 1 "[is_startOnline()]" Could not clean environment!
-			exit 1
-		}
-		exit 1
+	archfai_startStrapping "local" || {
+		clog 1  "[archfai_startLocal()]" Could not start bootstrapping.
+		return 1
 	}
-	
-	is_clean || {
-		clog 1 "[is_startOnline()]" Could not clean environment!
-		exit 1
-	}
-	
-	exit 0
-}
 
-archfai_help(){
-	cat << EOF
-Archfai help
-============
-usage: $0 	[ -h | -l | -v ]
-	v := Puts the current version on the console
-	h := Displays help
-	l := Toggles the local mode 
-		 (see https://github.com/forflo/archfai)
-EOF
-	return 0
-}
-
-archfai_version(){
-	cat << EOF
-Version: ${VERSION}
-===================
-Archfai aims to provide a shell script package
-specific for archlinux which makes it possible
-to automate the  complete installation process
-with just one command.
-EOF
 	return 0
 }
 
@@ -309,9 +253,15 @@ archfai_main(){
 	}
 
 	[ "$AF_RUNLOCAL" = "true" ] && {
-
+		archfai_startLocal || {
+			clog 1 "[archfai_main()]" archfai_startLocal failed!
+			return 1
+		}
 	} || {
-		
+		archfai_startOnline || {
+			clog 1 "[archfai_main()]" archfai_startOnline failed!
+			return 1
+		}
 	}
 
 	return 0
